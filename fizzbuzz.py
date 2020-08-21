d63003079fd05de992fc343266599d232fb68bf5
Janine Hempy <jhempy@gmail.com>
2020-08-21 00:07:53 -0400

    Cleaned up fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -16,22 +16,3 @@ for i in range(1,101,1):
 		if (is_multiple(i, key)):
 			message = message + checks[key]
 	print(message or i)
-
-
-
-
-
-
-
-	# binary = is_multiple(i,3) + is_multiple(i,5)
-	# decimal = int(binary, 2)
-	# if decimal == 3:
-	# 	print("fizzbuzz")
-	# elif decimal == 2:
-	# 	print("fizz")
-	# elif decimal == 1:
-	# 	print("buzz")
-	# else:
-	# 	print(i)
-
-	
