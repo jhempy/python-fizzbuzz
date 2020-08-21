484e9247ca1167e49dcea076764292fa04cad681
Janine Hempy <jhempy@gmail.com>
2020-08-21 00:02:31 -0400

    Improved fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,20 +4,34 @@
 
 def is_multiple(i,n):
 	if i % n == 0:
-		return "1"
+		return True
 	else:
-		return "0"
+		return False
+
+checks = { 3: "fizz", 5: "buzz"}
 
 for i in range(1,101,1):
-	binary = is_multiple(i,3) + is_multiple(i,5)
-	decimal = int(binary, 2)
-	if decimal == 3:
-		print("fizzbuzz")
-	elif decimal == 2:
-		print("fizz")
-	elif decimal == 1:
-		print("buzz")
-	else:
-		print(i)
+	message = ""
+	for key in checks:
+		if (is_multiple(i, key)):
+			message = message + checks[key]
+	print(message or i)
+
+
+
+
+
+
+
+	# binary = is_multiple(i,3) + is_multiple(i,5)
+	# decimal = int(binary, 2)
+	# if decimal == 3:
+	# 	print("fizzbuzz")
+	# elif decimal == 2:
+	# 	print("fizz")
+	# elif decimal == 1:
+	# 	print("buzz")
+	# else:
+	# 	print(i)
 
 	
